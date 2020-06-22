357a57a11be7a59dece01e285c022e6887092a6d
Alexey Korban <ldwx@mail.ru>
2020-06-22 19:19:23 +0500

    Add Calculator implementation

--- src/main/java/ru/ldwx/Calculator.java
+++ src/main/java/ru/ldwx/Calculator.java
@@ -1,13 +1,48 @@
 package ru.ldwx;
 
-import java.util.Queue;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 public class Calculator {
+    private final Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
     private final Parser parser = new Parser();
 
-    public double calculate(String input) {
-        Queue<String> operators = parser.parseExpression(input);
+    public Double calculate(String input) {
+        Deque<String> operators = parser.parseExpression(input);
+        Deque<Double> calculationResults = new LinkedList<>();
+        while (!operators.isEmpty()) {
+            String currentOperator = operators.pollFirst();
+            if (isNumeric(currentOperator)) {
+                calculationResults.offer(Double.parseDouble(currentOperator));
+            } else {
+                Double firstNumber = calculationResults.pollLast();
+                Double secondNumber = calculationResults.pollLast();
+                Double result = calc(firstNumber, secondNumber, currentOperator);
+                calculationResults.offer(result);
+            }
+        }
+        return calculationResults.poll();
+    }
 
-        return 0;
+    private Double calc(Double firstNumber, Double secondNumber, String currentOperator) {
+        switch (currentOperator) {
+            case "+":
+                return secondNumber + firstNumber;
+            case "-":
+                return secondNumber - firstNumber;
+            case "*":
+                return secondNumber * firstNumber;
+            case "/":
+                return secondNumber / firstNumber;
+        }
+        return 0d;
+    }
+
+    private boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
     }
 }
