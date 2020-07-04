60a3d9020f6eadc0e89bff719cdb328ed2ecd95b
Alexey Korban <ldwx@mail.ru>
2020-07-04 20:10:14 +0500

    Fix Application

--- src/main/java/ru/ldwx/Application.java
+++ src/main/java/ru/ldwx/Application.java
@@ -4,8 +4,7 @@ public class Application {
 
     public static void main(String[] args) throws ExpressionException {
         ConsoleHelper consoleHelper = new ConsoleHelper();
-        String expression = consoleHelper.getExpression();
-        Double result = new Calculator().calculate(expression);
+        Double result = new Calculator().calculate(consoleHelper.getExpression());
         consoleHelper.print(result.toString());
     }
 }
